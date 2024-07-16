e0383f2ea493e66fa7b55167957607033f06cac2
Keith Adams <kma@pebblebed.com>
2024-07-16 15:15:11 -0700

    Got something going did we not??

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <pqxx/pqxx>
 
 int main(int argc, char ** argv) {
+	if (argc <= 1) {
+		std::cerr << "usage: " << argv[0] << " <db_uri>\n";
+		return -1;
+	}
+	auto db_uri = argv[1];
+	pqxx::connection C(db_uri);
+	if (C.is_open()) {
+		std::cout << "success!\n";
+	} else {
+		std::cout << "failure :( \n";
+	}
 	std::cout << "woot!" << "\n";
 	std::cout << 3.14159 / 2 << "\n";
 	return 0;
