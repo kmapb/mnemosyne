14c9e197ccd279f2c6c34a49d233e33861b6de44
Keith Adams <kma@pebblebed.com>
2024-07-17 23:21:04 +0000

    Values get names.

--- test/test_value.cpp
+++ test/test_value.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 #include <pqxx/pqxx>
+#include <unordered_set>
+#include <string>
 
 #include "../value.hpp"
 
+using namespace std;
 using namespace mnem;
 
 // This is a trivial test that always passes
@@ -16,6 +19,17 @@ TEST(Dynamism, ValuePolymorphism) {
     v2 = Value::Int(123);
     EXPECT_EQ(v2.asInt(), 123);
     EXPECT_EQ(v1.asString(), std::string("she works hard for the monkeys!"));
+}
+
+TEST(Naming, ValueNamesDiffer) {
+    unordered_set<std::string> digests;
+
+    constexpr int const sz = 128;
+    for (auto i = 0; i < sz; i++) {
+        auto v = Value::Int(i);
+        digests.insert(v.name().toString());
+    }
+    EXPECT_EQ(digests.size(), sz);
 }
 
 int main(int argc, char **argv) {
